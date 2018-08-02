2c4ca79683228ea444a39bfa1dc34c52eeaca85f
venkateshmorishetty <venkateshmorishetty@msitprogram.net>
2018-08-02 19:22:33 +0530

    " -> 0 of 5 passed. pylint: 9.23/10 "

--- M4/p2/bob_counter.py
+++ M4/p2/bob_counter.py
@@ -5,12 +5,21 @@ For example, if s = 'azcbobobegghakl', then your program should print
 
 Number of times bob occurs is: 2'''
 
+def find_bob(string, index):
+    return string.find("bob", index)
+
 def main():
     ''' This function finds the number of occurances of bob'''
     string = input()
     # the input string is in s
     # remove pass and start your code here
-    print(string.count("bob"))
+    index = find_bob(string, 0)
+    count = 0
+    while index > 0:
+        print(index)
+        count += 1
+        index = find_bob(string, index+1)
+    print(count)
 
 if __name__ == "__main__":
     main()
