c49ba7481a6d104c63cad038ea4f34ef96e172cc
venkateshmorishetty <venkateshmorishetty@msitprogram.net>
2018-08-03 19:29:43 +0530

    " -> 3 of 3 passed. pylint: 10.00/10 "

--- day5/p4/square_root_newtonrapson.py
+++ day5/p4/square_root_newtonrapson.py
@@ -10,12 +10,13 @@
 # output: 6.999999999999991
 
 def main():
-	'''uses newton rapson method'''
-	number = int(input())
-	epsilon = 0.01
-	guess=number/2.0
-	while (guess**2-number)>=epsilon:
-		guess=guess-(((guess**2)-number)/(2*guess))
-	print(guess)	
-if __name__== "__main__":
-	main()
+    '''uses newton rapson method'''
+    number = int(input())
+    epsilon = 0.01
+    guess = number/2.0
+    while (guess**2-number) >= epsilon:
+        guess = guess-(((guess**2)-number)/(2*guess))
+    print(guess)
+if __name__ == "__main__":
+    main()
+    
