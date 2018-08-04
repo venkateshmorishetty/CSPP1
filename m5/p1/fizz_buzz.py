8e5e87d6f9d3a2fd81cd120e172854cdf8c23aa2
venkateshmorishetty <venkateshmorishetty@msitprogram.net>
2018-08-04 10:30:41 +0530

    " -> 4 of 7 passed. pylint: 10.00/10 "

--- m5/p1/fizz_buzz.py
+++ m5/p1/fizz_buzz.py
@@ -9,16 +9,14 @@ def main():
     Read number from the input, store it in variable num.
     '''
     num = int(input())
-    ans = ''
     for i in range(1, num+1, 1):
         if(i%3 == 0 and i%5 == 0):
-            ans += "FizzBuzz"
+            print("FizzBuzz")
         elif i%3 == 0:
-            ans += "Fizz"
+            print("Fizz")
         elif i%5 == 0:
-            ans += "Buzz"
+            print("Buzz")
         else:
-            ans += str(i)
-    print(ans)
+            print(i)
 if __name__ == "__main__":
     main()
