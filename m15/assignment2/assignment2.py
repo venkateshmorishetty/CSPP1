8d4431a9e89104b8c32adb146ba69840cef980a8
venkateshmorishetty <venkateshmorishetty@msitprogram.net>
2018-08-22 23:50:46 +0530

    " -> 1 of 1 passed. pylint: 9.31/10 "

--- m15/assignment2/assignment2.py
+++ m15/assignment2/assignment2.py
@@ -264,8 +264,8 @@ def decrypt_story():
     ''' Decrypt the story text using CiphertextMessage class and return the
         shift value and decrypted string in a tuple.
     '''
-    pass #delete this line when you write your code.
-
+    decrypt = CiphertextMessage(get_story_string())
+    return decrypt.decrypt_message()
 ### DO NOT MODIFY THIS METHOD ###
 def main():
     ''' This method is provided to handle testcases'''
