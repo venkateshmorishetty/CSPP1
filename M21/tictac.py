8ef3e3471810fe9e2e8c620ffee97e971a3812ac
venkateshmorishetty <venkateshmorishetty@msitprogram.net>
2018-08-24 13:25:55 +0530

    " -> 14 of 14 passed. pylint: 8.73/10 "

--- M21/tictac.py
+++ M21/tictac.py
@@ -1,29 +1,30 @@
+'''tic tac'''
 import copy
 def tictac():
     '''tictac implementation'''
     rank1 = 0 #this rank is for o
     rank2 = 0 #this rank is for x
-    n = []
+    tictac = []
     for i in range(0, 3, 1):
-        n += [input().split(' ')]
+        tictac += [input().split(' ')]
     count1 = 0
     count2 = 0
-    for itemp in n:
-        for jtemp in itemp: 
+    for itemp in tictac:
+        for jtemp in itemp:
             if jtemp != 'o' and jtemp != 'x' and jtemp != '.':
                 return "invalid input"
             if jtemp == 'o':
-                count1 += 1 
+                count1 += 1
             if jtemp == 'x':
                 count2 += 1
     if abs(count1 - count2) != 1:
         return "invalid game"
     diag1 = []
     diag2 = []
-    matrix2 = []    
-    for itemp in range(len(n)):
+    matrix2 = []
+    for itemp in range(len(tictac)):
         list2 = []
-        k = copy.deepcopy(n[itemp])
+        k = copy.deepcopy(tictac[itemp])
         temp = set(k)
         if len(temp) == 1:
             lis = list(temp)
@@ -32,14 +33,14 @@ def tictac():
             else:
                 rank2 = 1
         lis = []
-        k = []      
-        for jtemp in range(len(n[itemp])):
-            list2.append(n[jtemp][itemp])
+        k = []
+        for jtemp in range(len(tictac[itemp])):
+            list2.append(tictac[jtemp][itemp])
             if itemp == jtemp:
-                diag1.append(n[itemp][jtemp])
-            if itemp + jtemp == len(n) - 1:
-                diag2.append(n[itemp][jtemp])
-        matrix2.append(list2)      
+                diag1.append(tictac[itemp][jtemp])
+            if itemp + jtemp == len(tictac) - 1:
+                diag2.append(tictac[itemp][jtemp])
+        matrix2.append(list2)
     for itemp in range(len(matrix2)):
         k = copy.deepcopy(matrix2[itemp])
         temp = set(k)
