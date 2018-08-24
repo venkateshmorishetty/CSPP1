50fc963faefc9b1592e2a8364960d9d8a252f6dc
venkateshmorishetty <venkateshmorishetty@msitprogram.net>
2018-08-24 13:12:14 +0530

    " -> 14 of 14 passed. pylint: 3.94/10 "

--- M21/tictac.py
+++ M21/tictac.py
@@ -1,11 +1,11 @@
 import copy
 def tictac():
-    r1 = 0 #=====o
-    r2 = 0 #=====x
+    '''tictac implementation'''
+    r1 = 0 #this rank is for o
+    r2 = 0 #this rank is for x
     n = []
     for i in range(0,3,1):
         n += [input().split(' ')]
-    # print(n)
     count1 = 0
     count2 = 0
     for i in n:
@@ -16,17 +16,8 @@ def tictac():
                 count1 += 1 
             if j == 'x':
                 count2 += 1
-    # print(count1)
-    # print(count2)            
-    # if count1 != count2:
-    #     return "invalid game"
-    # if count2 != count1-1:
-    #     return "invalid game"                       
-
     if abs(count1 - count2)!=1:
         return "invalid game"
-
-    
     diag1 = []
     diag2 = []
     matrix2 = []    
@@ -43,13 +34,12 @@ def tictac():
         l = []
         k = []      
         for j in range(len(n[i])):
-            l2.append(n[j][i])      
+            l2.append(n[j][i])
             if i == j:
                 diag1.append(n[i][j])
             if i+j==len(n)-1:
                 diag2.append(n[i][j])
         matrix2.append(l2)      
-    # print("matrix2",matrix2)          
     for i in range(len(matrix2)):
         k = copy.deepcopy(matrix2[i])
         temp = set(k)
@@ -59,18 +49,14 @@ def tictac():
                 r1 =1
             elif l[0] == 'x':
                 r2 =1
-    # print("after transpose",r1)
-    # print("after transpose",r2)           
-
-                                                
     temp = set(diag1)
     if len(temp) == 1:
         l = list(temp)
         if l[0] == 'o':
             r1 = 2
         else:
-            r2 = 2      
-    temp = set()        
+            r2 = 2
+    temp = set()
     temp = set(diag2)
     if len(temp) == 1:
         l = list(temp)
@@ -78,23 +64,13 @@ def tictac():
             r1 = 2
         else:
             r2 = 2
-
-    # print(r1)
-    # print(r2)         
     if r1>r2:
         return 'o'
     if r1==r2:
         return "draw"
     else:
         return 'x'
-
-
-
-
-
-
 def main():
     print(tictac())
-
 if __name__ == "__main__":
     main()
