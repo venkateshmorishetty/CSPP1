c26f698b221c4c312760c6ae776fb1f0fa62209d
venkateshmorishetty <venkateshmorishetty@msitprogram.net>
2018-08-24 13:17:45 +0530

    " -> 11 of 14 passed. pylint: 5.63/10 "

--- M21/tictac.py
+++ M21/tictac.py
@@ -1,72 +1,72 @@
 import copy
 def tictac():
     '''tictac implementation'''
-    r1 = 0 #this rank is for o
-    r2 = 0 #this rank is for x
+    rank1 = 0 #this rank is for o
+    rank2 = 0 #this rank is for x
     n = []
     for i in range(0,3,1):
         n += [input().split(' ')]
     count1 = 0
     count2 = 0
-    for i in n:
-        for j in i: 
-            if j != 'o' and j != 'x' and j != '.':
+    for itemp in n:
+        for jtemp in itemp: 
+            if jtemp != 'o' and jtemp != 'x' and jtemp != '.':
                 return "invalid input"
-            if j == 'o':
+            if jtemp == 'o':
                 count1 += 1 
-            if j == 'x':
+            if jtemp == 'x':
                 count2 += 1
     if abs(count1 - count2)!=1:
         return "invalid game"
     diag1 = []
     diag2 = []
     matrix2 = []    
-    for i in range(len(n)):
-        l2 = []
-        k = copy.deepcopy(n[i])
+    for itemp in range(len(n)):
+        list2 = []
+        k = copy.deepcopy(n[itemp])
         temp = set(k)
         if len(temp) ==1:
-            l = list(temp)
-            if l[0] == 'o':
-                r1 =1
+            lis = list(temp)
+            if lis[0] == 'o':
+                rank1 =1
             else:
-                r2 =1
-        l = []
+                rank2 =1
+        lis = []
         k = []      
-        for j in range(len(n[i])):
-            l2.append(n[j][i])
-            if i == j:
-                diag1.append(n[i][j])
-            if i+j==len(n)-1:
-                diag2.append(n[i][j])
-        matrix2.append(l2)      
-    for i in range(len(matrix2)):
-        k = copy.deepcopy(matrix2[i])
+        for jtemp in range(len(n[itemp])):
+            list2.append(n[jtemp][itemp])
+            if itemp == jtemp:
+                diag1.append(n[itemp][jtemp])
+            if itemp+jtemp==len(n)-1:
+                diag2.append(n[itemp][jtemp])
+        matrix2.append(list2)      
+    for itemp in range(len(matrix2)):
+        k = copy.deepcopy(matrix2[itemp])
         temp = set(k)
         if len(temp) ==1:
             l = list(temp)
-            if l[0] == 'o':
-                r1 =1
-            elif l[0] == 'x':
-                r2 =1
+            if lis[0] == 'o':
+                rank1 =1
+            elif lis[0] == 'x':
+                rank2 =1
     temp = set(diag1)
     if len(temp) == 1:
-        l = list(temp)
-        if l[0] == 'o':
-            r1 = 2
+        lis = list(temp)
+        if lis[0] == 'o':
+            rank1 = 2
         else:
-            r2 = 2
+            rank2 = 2
     temp = set()
     temp = set(diag2)
     if len(temp) == 1:
-        l = list(temp)
-        if l[0] == 'o':
-            r1 = 2
+        lis = list(temp)
+        if lis[0] == 'o':
+            rank1 = 2
         else:
-            r2 = 2
-    if r1>r2:
+            rank2 = 2
+    if rank1>rank2:
         return 'o'
-    if r1==r2:
+    if rank1==rank2:
         return "draw"
     else:
         return 'x'
