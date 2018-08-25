21a4cf07d1d7b05bd079bde8281d1a56029337c8
venkateshmorishetty <venkateshmorishetty@msitprogram.net>
2018-08-25 13:14:11 +0530

    " -> 0 of 3 passed. pylint: 5.00/10 "

--- m22/assignment3/tokenize.py
+++ m22/assignment3/tokenize.py
@@ -3,28 +3,23 @@ Write a function to tokenize a given string and return a dictionary with the fre
 each word
 '''
 import re
-dic = {}
-
+dictionary = {}
 def tokenize(string):
-	# string1 = re.sub('[^a-zA-Z0-9]','',string)
-	
-	list1 = string.split(' ')
-	for index in list1:
-		index1 = re.sub('[^a-zA-Z0-9]','',index)
-		if index1 in dic:
-			dic[index1] += 1
-		else:
-			dic[index1] = 1
-				
-
-    
-            
+    '''tokenize'''
+    # string1 = re.sub('[^a-zA-Z0-9]','',string)
+    list1 = string.split(' ')
+    for index_ in list1:
+        index1 = re.sub('[^a-zA-Z0-9]', '', index_)
+        if index1 in dictionary:
+            dictionary[index1] += 1
+        else:
+            dictionary[index1] = 1
 def main():
-    n = int(input())
-    for index in range(0, n, 1):
-    	string = input()
-    	tokenize(string)
-    print(dic)	
-
+    '''main'''
+    lines = int(input())
+    for _ in range(0, lines, 1):
+        string = input()
+        tokenize(string)
+    print(dictionary)
 if __name__ == '__main__':
     main()
